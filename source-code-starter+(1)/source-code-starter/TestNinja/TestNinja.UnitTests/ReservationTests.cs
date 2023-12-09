e1f4a9d3b9dfd1a8750122dfe5185d1b6c4ef8d3
WadeLiu <w0652039@gmail.com>
2023-12-09 11:24:00 +0800

    練習測試

---
+++ source-code-starter+(1)/source-code-starter/TestNinja/TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+using System;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            // 3A Pattern
+            // Arrange 安排 - 初始化物件
+            var reservation = new Reservation();
+
+            // Act 行動 - 執行要測試的方法
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            // Assert 斷言 - 驗證結果是否正確
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_SameUserCancellingTheReservation_ReturnsTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_AnotherUserCancellingReservation_ReturnsFalse()
+        {
+            var reservation = new Reservation { MadeBy = new User() };
+
+            var result = reservation.CanBeCancelledBy(new User());
+
+            Assert.IsFalse(result);
+        }
+    }
+}
