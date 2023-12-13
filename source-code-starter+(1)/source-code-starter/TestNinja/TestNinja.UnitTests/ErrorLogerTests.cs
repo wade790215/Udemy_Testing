0a19bd14c061ec42a84238e0db08e2865970e31d
WadeLiu <w0652039@gmail.com>
2023-12-13 08:32:40 +0800

    異常測試的寫法

---
+++ source-code-starter+(1)/source-code-starter/TestNinja/TestNinja.UnitTests/ErrorLogerTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLogerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            var logger = new ErrorLogger();
+
+            //logger.Log(null);
+            //logger.Log("");
+            //logger.Log(" ");
+
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+            //Assert.That(() => logger.Log(""), Throws.Exception.TypeOf<DivideByZeroException>());
+        }
+    }
+}
